a8af2a4cda719531c4ac180767bcb796f967cf37
Rodslater <91638825+Rodslater@users.noreply.github.com>
2022-03-10 12:15:07 -0300

    Update nifty50_scraping.R

--- nifty50_scraping.R
+++ nifty50_scraping.R
@@ -1,40 +1,24 @@
-#load libs
-
 library(tidyverse)
-library(rvest)
-library(janitor)
+library(readxl)
+library(curl)
 
-#nse top gainers
 
-url <- 'https://www.moneycontrol.com/stocks/marketstats/nsegainer/index.php'
+url <- "https://www.gov.br/anp/pt-br/assuntos/precos-e-defesa-da-concorrencia/precos/precos-revenda-e-de-distribuicao-combustiveis/shlp/semanal/semanal-estados-desde-2013.xlsx"
+destfile <- "semanal_estados_desde_2013.xlsx"
+curl_download(url, destfile)
+estadual_semanal<- read_excel(destfile, 
+                              col_types = c("date", "date", "text", "text", 
+                                            "text", "numeric", "skip", "numeric", 
+                                            "skip", "numeric", "numeric", "skip", 
+                                            "skip", "numeric", "skip", "skip", 
+                                            "skip", "skip"), skip = 16)
 
-# extract html 
 
-url_html <- read_html(url)
+colnames(estadual_semanal) <- c('datai','dataf', 'regiao', 'estado', 
+                                'combustivel', 'npostos', 'pmediorevenda', 
+                               'pminrevenda', 'pmaxrevenda', 'pmediodist')
 
-#table extraction
 
-url_tables <- url_html %>% html_table(fill = TRUE)
-
-#extract relevant table
-
-top_gainers <- url_tables[[2]]
-
-#extract relevant columns
-
-top_gainers %>%
-  select(1:7) -> top_gainers
-
-top_gainers %>% 
-  clean_names() -> top_gainers
-
-top_gainers %>%
-  filter(!is.na(low)) -> top_gainers
-
-top_gainers %>%
-  separate(company_name,
-           into = 'company_name',
-           sep = '\t') -> top_gainers
-           
-           
- write_csv(top_gainers,paste0('data/',Sys.Date(),'_top_gainers','.csv'))
+#Exportar rds
+path_rds <- 'estadual_semanal.rds'
+saveRDS(estadual_semanal, "estadual_semanal.rds")
